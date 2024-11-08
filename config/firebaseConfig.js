73b69dc90f74b6d7980f6e3302c268ed0f37ab9e
Brian Kimathi <98356481+BrianKimathi@users.noreply.github.com>
2024-11-08 11:14:31 +0300

    Update firebaseConfig.js

--- config/firebaseConfig.js
+++ config/firebaseConfig.js
@@ -1,6 +1,6 @@
 var admin = require("firebase-admin");
 
-var serviceAccount = require("./service-account.json");
+var serviceAccount = require("/etc/secrets/service-account.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
